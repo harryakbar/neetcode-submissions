7715d490ec80a797cef14d23d69b34cf1846fc29
Harry Akbar Ali Munir <harryakbaram@gmail.com>
2026-04-22 11:27:54 +0800

    Add: anagram-groups - submission-4

---
+++ Data Structures & Algorithms/anagram-groups/submission-4.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        let group = new Map()
+
+        for (let str of strs) {
+            const key = str.split("").sort().join("")
+
+            if (group.has(key)) {
+                group.set(key, [...group.get(key), str])
+            } else {
+                group.set(key, [str])
+            }
+        }
+
+        return [...group.values()]
+    }
+}
