18b01626e4a53033f7d611e38f40256659ecfaa6
Harry Akbar Ali Munir <harryakbaram@gmail.com>
2026-08-10 15:55:43 +0800

    Add: longest-substring-without-duplicates - submission-26

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-26.ts
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    lengthOfLongestSubstring(s: string): number {
+         let left = 0;
+        let right = 0;
+
+        let max = 0;
+        let substring = new Set();
+        while (right < s.length) {
+
+            while (substring.has(s[right])) {
+                substring.delete(s[left])
+                left += 1;
+            }
+
+            substring.add(s[right])
+            max = Math.max(max, right-left +1)
+
+            right += 1
+        }
+
+        return max
+    }
+}
